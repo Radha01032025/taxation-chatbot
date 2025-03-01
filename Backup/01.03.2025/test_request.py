e03610ebd70d13da699731b56957caa03e4534d0
Amit Bhadra Srivastava <srivastavaa1993@gmail.com>
2025-03-01 20:55:49 +0530

    Initial commit

--- Backup/01.03.2025/test_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-query = "calculate GST"
-response = requests.get(f"http://127.0.0.1:8000/tax_chatbot?query={query}")
-
-if response.status_code == 200:
-    print(response.json())
-else:
-    print("Error:", response.status_code, response.text)
